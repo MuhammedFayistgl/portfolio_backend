ca9d32cbe85b52fa26fff5c66823d93974d177b0
MuhammedFayistgl <77223454+MuhammedFayistgl@users.noreply.github.com>
2023-09-14 16:27:01 +0530

    session baced authentication remove

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -52,11 +52,11 @@ export const Login = async (req, res, next) => {
 		const token = createSecretToken(user._id);
 		res.cookie("token", token, {
 			withCredentials: true,
-			httpOnly: false,
+			httpOnly: true,
+			secure:true,
 			path: "/",
-			SameSite:"None",
-			secure:false,
-			// domain:'localhost'
+			SameSite: "None",
+			domain:'localhost'
 		});
 		res.status(201).json({
 			message: "User logged in successfully",
